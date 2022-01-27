59b4f993bc023c661f1232b98b9db14510cb12e0
petidesoftwares <petideenterprise@gmail.com>
2022-01-27 15:57:36 +0100

    UI updated

--- uis/home.php
+++ uis/home.php
@@ -4,12 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--        <link rel="stylesheet" href="../statics/bootstrap-5.1.3-dist/css/bootstrap.css">-->
-    <link rel="stylesheet" href="../statics/css/ssm.css">
+    <link rel="stylesheet" href="../../statics/css/ssm.css">
 </head>
 <body>
 <div class="row header-pane">
-    <div class="col-2 logo"></div>
-    <div class="col-10 app-title">Smart School Manager</div>
+    <div class="logged-in-header">
+        <div class="col-2 logo"></div>
+        <div class="col-10 app-title">Smart School Manager</div>
+        <div class="col-12 navbar-top">
+            <img id="menu-icon" src="../../statics/images/menu_white_24dp.svg">
+            <div id="user-icon">
+                <table>
+                    <tr>
+                        <td><img src="../../statics/images/icons/avater.png" width="35px"></td>
+                        <td>Mr. P. O.<br> Ide</td>
+                    </tr>
+                </table>
+            </div>
+            <div id="find-pane"><img src="../../statics/images/icons/search_white_24dp.svg" height="20px" width="25px"><input type="text" name="search" style="border: solid 1px; padding-bottom: 0px; margin-bottom: 0px; height: 30px"></div>
+
+        </div>
+    </div>
 </div>
 <div class="col-12">
     <div class="col-3 side-bar">side bar</div>
