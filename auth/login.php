7705d496cca2fec4020c9391157421ad73d3bfc3
petidesoftwares <petideenterprise@gmail.com>
2022-01-27 13:05:44 +0100

    initial commit

---
+++ auth/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--        <link rel="stylesheet" href="../statics/bootstrap-5.1.3-dist/css/bootstrap.css">-->
+        <link rel="stylesheet" href="../statics/css/ssm.css">
+    </head>
+    <body>
+        <div class="row header-pane">
+            <div class="col-2 logo"></div>
+            <div class="col-10 app-title">Smart School Manager</div>
+        </div>
+        <div class="col-12">
+            <div class="col-12">
+                <div class="col-3"></div>
+                <div class="col-6 form-pane">
+                    <form class="auth-form">
+                        <h4>LOGIN</h4>
+                        <input type="text" name="username" placeholder="Enter Username or Email" class="form-input form-element">
+                        <input type="text" name="user-password" placeholder="Enter Password" class="form-input form-element">
+                        <input type="submit" name="submit" value="LOGIN" class="form-input auth-btn"><span id="forget-password-link"><a href="">Forget Password</a></span>
+                    </form>
+                </div>
+                <div class="col-3"></div>
+            </div>
+<!--            <div class="col-12 footer">footer</div>-->
+        </div>
+
+    </body>
+</html>
---
+++ uis/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--        <link rel="stylesheet" href="../statics/bootstrap-5.1.3-dist/css/bootstrap.css">-->
+    <link rel="stylesheet" href="../statics/css/ssm.css">
+</head>
+<body>
+<div class="row header-pane">
+    <div class="col-2 logo"></div>
+    <div class="col-10 app-title">Smart School Manager</div>
+</div>
+<div class="col-12">
+    <div class="col-3 side-bar">side bar</div>
+    <div class="col-9">
+        <div class="main-content">Content</div>
+        <div class="col-12 footer">footer</div>
+    </div>
+
+</div>
+
+</body>
+</html>
