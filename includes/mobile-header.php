3e1373863b4e0adaf2d4266c5bbd68f1117d7e2d
petidesoftwares <petideenterprise@gmail.com>
2022-01-28 16:39:15 +0100

    Update mobile-header.php

--- includes/mobile-header.php
+++ includes/mobile-header.php
@@ -1,4 +1,4 @@
-<img id="menu-icon" src="../../statics/images/menu_white_24dp.svg">
+<img id="menu-icon" src="../../statics/images/menu_white_24dp.svg" onclick="showSideBarMenu()">
 <div class="app-title" id="app-title-mobile">Smart School Manager</div>
 <div class="logo" id="logo-mobile"></div>
 <div id="mobile-find-pane"><img src="../../statics/images/icons/search_white_24dp.svg"><input type="text" name="search" style="border: solid 1px; padding-bottom: 0px; margin-bottom: 0px; width: 92%"></div>
