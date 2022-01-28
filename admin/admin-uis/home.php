6be5d3aff147f8c0cce5ac4c3b343f948e99c349
petidesoftwares <petideenterprise@gmail.com>
2022-01-28 15:49:55 +0100

    Update home.php

--- admin/admin-uis/home.php
+++ admin/admin-uis/home.php
@@ -5,24 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--        <link rel="stylesheet" href="../statics/bootstrap-5.1.3-dist/css/bootstrap.css">-->
     <link rel="stylesheet" href="../../statics/css/ssm.css">
+    <script type="text/javascript" src="../../statics/js/jquery-3.6.0.min.js"></script>
+    <script type="text/javascript" src="../../statics/js/ssm-screen-resolution.js"></script>
 </head>
 <body>
 <div class="row header-pane">
     <div class="logged-in-header">
-        <div class="col-2 logo"></div>
-        <div class="col-10 app-title">Smart School Manager</div>
-        <div class="col-12 navbar-top">
-            <img id="menu-icon" src="../../statics/images/menu_white_24dp.svg">
-            <div id="user-icon">
-                <table>
-                    <tr>
-                        <td><img src="../../statics/images/icons/avater.png" width="35px"></td>
-                        <td>Mr. P. O.<br> Ide</td>
-                    </tr>
-                </table>
-            </div>
-            <div id="find-pane"><img src="../../statics/images/icons/search_white_24dp.svg" height="20px" width="25px"><input type="text" name="search" style="border: solid 1px; padding-bottom: 0px; margin-bottom: 0px; height: 30px"></div>
-
+        <div id="mobile-header">
+            <?php include_once ("../../includes/mobile-header.php")?>
+        </div>
+        <div id="desktop-header">
+            <?php include_once ("../../includes/desktop-header.php")?>
         </div>
     </div>
 </div>
