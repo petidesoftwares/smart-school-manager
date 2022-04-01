aeb66cd31db84a68397452b38a544e57ef5b97bb
petidesoftwares <petideenterprise@gmail.com>
2022-04-01 12:42:06 +0100

    updated

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,24 +1,24 @@
 <?php return array (
   'root' => 
   array (
-    'pretty_version' => '1.0.0+no-version-set',
-    'version' => '1.0.0.0',
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
     'aliases' => 
     array (
     ),
-    'reference' => NULL,
+    'reference' => '8c527229ce750d2d82a23ae7ccbd567eae6b4dd5',
     'name' => 'dell/ssm',
   ),
   'versions' => 
   array (
     'dell/ssm' => 
     array (
-      'pretty_version' => '1.0.0+no-version-set',
-      'version' => '1.0.0.0',
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
       'aliases' => 
       array (
       ),
-      'reference' => NULL,
+      'reference' => '8c527229ce750d2d82a23ae7ccbd567eae6b4dd5',
     ),
   ),
 );
