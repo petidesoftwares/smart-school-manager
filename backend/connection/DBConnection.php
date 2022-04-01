0810b6aaa40784e8c26ec99738a8e5541a57638a
petidesoftwares <petideenterprise@gmail.com>
2022-04-01 10:07:44 +0100

    Update DBConnection.php

--- backend/connection/DBConnection.php
+++ backend/connection/DBConnection.php
@@ -5,21 +5,27 @@ use mysqli;
 
 class DBConnection
 {
-//$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-//$cleardb_server = $cleardb_url["host"];
-//$cleardb_username = $cleardb_url["user"];
-//$cleardb_password = $cleardb_url["pass"];
-//$cleardb_db = substr($cleardb_url["path"],1);
+    private $cleardb_url;
+    private $cleardb_server;
+    private $cleardb_username;
+    private $cleardb_password;
+    private $cleardb_db;
 
-    const SERVERNAME = "us-cdbr-east-05.cleardb.net";
-    const USERNAME = "bf010ca2e782f7";
-    const PASSWORD = "7c05bc7e";
-    CONST DB = "heroku_f356ae46764aba9";
+//    const SERVERNAME = "us-cdbr-east-05.cleardb.net";
+//    const USERNAME = "bf010ca2e782f7";
+//    const PASSWORD = "7c05bc7e";
+//    CONST DB = "heroku_f356ae46764aba9";
 
     public $active_group;
     public $query_builder;
 
     public function __construct(){
+        $this->cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $this->cleardb_server = $this->cleardb_url["host"];
+        $this->cleardb_username = $this->cleardb_url["user"];
+        $this->cleardb_password = $this->cleardb_url["pass"];
+        $this->cleardb_db = substr($this->cleardb_url["path"],1);
+
         $this->active_group = 'default';
         $this->query_builder = FALSE;
     }
