42fe4a928a1c49c4827e16cfa68e8257995b49f6
petidesoftwares <petideenterprise@gmail.com>
2022-04-01 10:41:01 +0100

    server testing update

--- backend/App/get-all-students.php
+++ backend/App/get-all-students.php
@@ -8,7 +8,8 @@ include_once ("../../vendor/autoload.php");
 if(isset($_POST)){
     $conn = new DBConnection();
     $con = $conn->connect();
+    echo $con;
 
-    $pupils = new AdminController();
-    echo $pupils->getAllPupils($con);
+//    $pupils = new AdminController();
+//    echo $pupils->getAllPupils($con);
 }
