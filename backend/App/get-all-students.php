0deef00534b3903369decf6cab442c5fec7d6b29
petidesoftwares <petideenterprise@gmail.com>
2022-04-01 13:49:05 +0100

    updeated

--- backend/App/get-all-students.php
+++ backend/App/get-all-students.php
@@ -2,13 +2,18 @@
 
 use Backend\Connection\DBConnection;
 use Backend\Controllers\AdminController;
+use Backend\Models\Model;
 
 include_once ("../../vendor/autoload.php");
 
 if(isset($_POST)){
     $conn = new DBConnection();
     $con = $conn->connect();
-    
-    $pupils = new AdminController();
-    echo $pupils->getAllPupils($con);
+
+//    $pupils = new AdminController();
+//    echo $pupils->getAllPupils($con);
+
+    $pupils = new Pupil();
+    $othername = new PupilOthername();
+    echo $pupils->allWithRelationship($con,$othername->getTable());
 }
--- backend/controllers/AdminController.php
+++ backend/controllers/AdminController.php
@@ -1,6 +1,4 @@
 <?php
-
-
 namespace Backend\Controllers;
 
 
