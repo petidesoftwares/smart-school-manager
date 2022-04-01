5e9553c65831b5903b822af8a657d36b21dddc65
petidesoftwares <petideenterprise@gmail.com>
2022-04-01 11:04:38 +0100

    Update get-all-students.php

--- backend/App/get-all-students.php
+++ backend/App/get-all-students.php
@@ -8,7 +8,7 @@ include_once ("../../vendor/autoload.php");
 if(isset($_POST)){
     $conn = new DBConnection();
     $con = $conn->connect();
-    echo $con;
+    print_r($con);
 
 //    $pupils = new AdminController();
 //    echo $pupils->getAllPupils($con);
